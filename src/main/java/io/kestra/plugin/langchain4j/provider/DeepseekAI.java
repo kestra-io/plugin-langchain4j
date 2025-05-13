47a323dd2f336331476c920de36492f465884da1
rkampani <rkampani@gmail.com>
2025-05-13 08:17:56 -0500

    adding deepseek integration

---
+++ src/main/java/io/kestra/plugin/langchain4j/provider/DeepseekAI.java
@@ -0,0 +1,73 @@
+package io.kestra.plugin.langchain4j.provider;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.model.image.ImageModel;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.model.openai.OpenAiEmbeddingModel;
+import dev.langchain4j.model.openai.OpenAiImageModel;
+import io.kestra.core.exceptions.IllegalVariableEvaluationException;
+import io.kestra.core.models.annotations.Plugin;
+import io.kestra.core.models.property.Property;
+import io.kestra.core.runners.RunContext;
+import io.kestra.plugin.langchain4j.domain.ChatConfiguration;
+import io.kestra.plugin.langchain4j.domain.ModelProvider;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@Plugin(beta = true)
+@JsonDeserialize
+@Schema(
+    title = "Deepseek Model Provider"
+)
+public class DeepseekAI extends ModelProvider {
+    @Schema(title = "API Key")
+    @NotNull
+    private Property<String> apiKey;
+
+    @Schema(title = "API base URL")
+    private Property<String> baseUrl;
+
+    @Override
+    public ChatLanguageModel chatLanguageModel(RunContext runContext, ChatConfiguration configuration) throws IllegalVariableEvaluationException {
+        if (configuration.getTopK() != null) {
+            throw new IllegalArgumentException("OpenAI models didn't support topK");
+        }
+
+        return OpenAiChatModel.builder()
+            .modelName(runContext.render(this.getModelName()).as(String.class).orElseThrow())
+            .apiKey(runContext.render(this.apiKey).as(String.class).orElseThrow())
+            .temperature(runContext.render(configuration.getTemperature()).as(Double.class).orElse(null))
+            .topP(runContext.render(configuration.getTopP()).as(Double.class).orElse(null))
+            .baseUrl(runContext.render(baseUrl).as(String.class).orElse(null))
+            .build();
+    }
+
+    @Override
+    public ImageModel imageModel(RunContext runContext) throws IllegalVariableEvaluationException {
+        return OpenAiImageModel.builder()
+            .modelName(runContext.render(this.getModelName()).as(String.class).orElseThrow())
+            .apiKey(runContext.render(this.apiKey).as(String.class).orElseThrow())
+            .baseUrl(runContext.render(baseUrl).as(String.class).orElse(null))
+            .build();
+    }
+
+    @Override
+    public EmbeddingModel embeddingModel(RunContext runContext) throws IllegalVariableEvaluationException {
+        return OpenAiEmbeddingModel.builder()
+            .modelName(runContext.render(this.getModelName()).as(String.class).orElseThrow())
+            .apiKey(runContext.render(this.apiKey).as(String.class).orElseThrow())
+            .baseUrl(runContext.render(baseUrl).as(String.class).orElse(null))
+            .build();
+    }
+
+}
