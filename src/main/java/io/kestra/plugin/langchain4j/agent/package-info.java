87491718abb082533e2d9e2d705bd5d6b8a1d3b4
Loïc Mathieu <loikeseke@gmail.com>
2025-06-26 18:08:08 +0200

    feat: AIAgent task with a memory

---
+++ src/main/java/io/kestra/plugin/langchain4j/agent/package-info.java
@@ -0,0 +1,8 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks for AI Agent for Kestra.\n" +
+        "It uses the Langchain4j framework.",
+    categories = PluginSubGroup.PluginCategory.AI
+)
+package io.kestra.plugin.langchain4j.agent;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
