c497586da6301baae6579176ca024bc3bfd80cda
Loïc Mathieu <loikeseke@gmail.com>
2025-08-21 16:22:50 +0200

    chore: add a test with a flow description for the KestraFlowCalling tool

--- src/test/java/io/kestra/plugin/ai/tool/KestraFlowCallingTest.java
+++ src/test/java/io/kestra/plugin/ai/tool/KestraFlowCallingTest.java
@@ -84,8 +84,68 @@ class KestraFlowCallingTest {
         // check that an execution has been created
         var executions = executionRepository.findByFlowId(null, "company.team", "hello-world", Pageable.UNPAGED);
         assertThat(executions).hasSize(1);
+        assertThat(output.getCompletion()).contains(executions.getFirst().getId());
 
         flowRepository.delete(flow);
+        executionRepository.delete(executions.getFirst());
+    }
+
+    @Test
+    void descriptionFromTheFlow() throws Exception {
+        String flowYaml = """
+            id: hello-world-with-description
+            namespace: company.team
+            description: A flow that say Hello World
+
+            tasks:
+              - id: hello
+                type: io.kestra.plugin.core.log.Log
+                message: Hello World! 🚀
+            """;
+        var flow = flowRepository.create(GenericFlow.fromYaml(null, flowYaml));
+
+        RunContext runContext = runContextFactory.of(Map.of(
+            "apiKey", "demo",
+            "modelName", "gpt-4o-mini",
+            "baseUrl", "http://langchain4j.dev/demo/openai/v1"
+        ));
+
+        var chat = ChatCompletion.builder()
+            .provider(OpenAI.builder()
+                .type(OpenAI.class.getName())
+                .apiKey(Property.ofExpression("{{ apiKey }}"))
+                .modelName(Property.ofExpression("{{ modelName }}"))
+                .baseUrl(Property.ofExpression("{{ baseUrl }}"))
+                .build()
+            )
+            .tools(List.of(
+                KestraFlowCalling.builder().namespace(Property.ofValue("company.team")).flowId(Property.ofValue("hello-world-with-description")).build())
+            )
+            .messages(Property.ofValue(
+                List.of(
+                    ChatCompletion.ChatMessage.builder().type(ChatCompletion.ChatMessageType.SYSTEM).content("You are an AI agent, please use the provided tool to fulfill the request.").build(),
+                    ChatCompletion.ChatMessage.builder().type(ChatCompletion.ChatMessageType.USER).content("I want to execute a flow to say Hello World.").build()
+                )))
+            // Use a low temperature and a fixed seed so the completion would be more deterministic
+            .configuration(ChatConfiguration.builder().temperature(Property.ofValue(0.1)).seed(Property.ofValue(123456789)).build())
+            .build();
+
+        var output = chat.run(runContext);
+        assertThat(output.getCompletion()).contains("success");
+        assertThat(output.getToolExecutions()).isNotEmpty();
+        assertThat(output.getToolExecutions()).extracting("requestName").contains("kestra_flow_company_team_hello-world-with-description");
+        assertThat(output.getIntermediateResponses()).isNotEmpty();
+        assertThat(output.getIntermediateResponses().getFirst().getFinishReason()).isEqualTo(FinishReason.TOOL_EXECUTION);
+        assertThat(output.getIntermediateResponses().getFirst().getToolExecutionRequests()).isNotEmpty();
+        assertThat(output.getIntermediateResponses().getFirst().getToolExecutionRequests().getFirst().getName()).isEqualTo("kestra_flow_company_team_hello-world-with-description");
+        assertThat(output.getIntermediateResponses().getFirst().getRequestDuration()).isNotNull();
+
+        // check that an execution has been created
+        var executions = executionRepository.findByFlowId(null, "company.team", "hello-world-with-description", Pageable.UNPAGED);
+        assertThat(executions).hasSize(1);
+
+        flowRepository.delete(flow);
+        executionRepository.delete(executions.getFirst());
     }
 
     @Test
